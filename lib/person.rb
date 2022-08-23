11a0ba640b654fef7fecac19a4e33c121d2144b7
Michael Okoe <michael.okoejr@gmail.com>
2022-08-23 01:22:27 +0000

    commit tree 3ca599dbdd390522d4c97d7882e70da059b7bfa7

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+# Creating a class of 'Person'
+
+class Person
+  # creating an instance of 'talk'
+
+  def talk
+    puts "Hello World!"
+  end
+end
+
+# Creating an instance of 'walk' in the 'Person' class
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+end
