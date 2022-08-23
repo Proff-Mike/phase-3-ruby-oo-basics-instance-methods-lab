d71fae466c6acde453b02188dd8abf54a442847d
Michael Okoe <michael.okoejr@gmail.com>
2022-08-23 01:23:09 +0000

    Completed Assignment

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+# Creating a "Dog" class
+
+class Dog
+  # Creating an instance of 'bark'
+  def bark
+    puts "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+
+# Creating an instance of 'sit'
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
